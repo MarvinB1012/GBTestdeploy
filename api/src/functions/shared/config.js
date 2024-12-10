90ebe00693958dfc4f2790e56a7a929a4878ed1a
MarvinB1012 <Berishamarvin@gmail.com>
2024-12-11 00:56:13 +0100

    djsk

--- api/src/functions/shared/config.js
+++ api/src/functions/shared/config.js
@@ -7,7 +7,7 @@ const allowedOrigins = [
 const corsHeaders = (origin) => ({
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : allowedOrigins[0],
-  'Access-Control-Allow-Methods': 'GET',
+  'Access-Control-Allow-Methods': 'GET, PATCH, POST',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization, Accept',
   'Access-Control-Max-Age': '86400',
   'Cache-Control': 'no-cache'
