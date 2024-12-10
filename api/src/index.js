ebc6266648a85bbc6ebb274161bbb32ff890afc8
MarvinB1012 <Berishamarvin@gmail.com>
2024-12-11 00:39:39 +0100

    fjsk

--- api/src/index.js
+++ api/src/index.js
@@ -8,8 +8,8 @@ require('./functions/notifications.js');
 require('./functions/room-data.js');
 require('./functions/room-sensor-data.js');
 require('./functions/sensor-data.js');
-require('./functions/settings.js');
-require('./functions/test.js');
+//require('./functions/settings.js');
+/*require('./functions/test.js');*/
 
 
 module.exports = app;
